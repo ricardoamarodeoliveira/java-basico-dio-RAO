c3edf5ba3700316d274076705c7ba2827240b4ab
Ricardo Amaro <ricardoamarofoto@gmail.com>
2024-06-06 21:06:33 -0300

    aula

--- banco-RAO-java-basico-DIO/src/ContaTerminal.java
+++ banco-RAO-java-basico-DIO/src/ContaTerminal.java
@@ -1,31 +1,32 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        /*String nome = args [0];
-        String sobrenome = args [1];ri
-        int idade = Integer.valueOf(args [2]);
-        double altura = Double.valueOf(args[3]);
-        */
-
-        System.out.println("Por favor digite seu nome: ");
+        System.out.println("Por favor digite seu primeiro nome: ");
         String nomeCliente = scanner.next(); 
+
+        System.out.println("Por favor digite seu ultimo sobrenome: ");
+        String sobrenomeCliente = scanner.next(); 
         
-        System.out.println("Por favor digite o número da Agência: ");
+        System.out.println("Senhor " + nomeCliente + " por favor digite o número da Agência: ");
         String agencia = scanner.next();
         
-        System.out.println("Por favor digite o numero da conta: ");
-        int numero = scanner.nextInt();
-       
+        System.out.println("Por favor, agora digite o numero da conta: ");
+        int numero = scanner.nextInt();      
                
 
-        System.out.println("Por favor informe o valor do seu depósito inicial: ");
+        System.out.println("Agora informe o valor do seu depósito inicial: ");
         double saldo = scanner.nextDouble();
 
-        System.out.println("Olá senhor " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é: "
+        System.out.println("Olá senhor " + nomeCliente + " " + sobrenomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é: "
         + agencia + ", conta: " + numero + ", e seu saldo no valor de R$ " + saldo + " já esta disponivel para saque.");
+        
 
     }
 }
